0b2d120bfc318336367be4b769fe62bba062167f
Shami_Mourya <shamimourya9852@gmail.com>
2023-08-12 18:10:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
+import Dashboard from './Pages/Dashboard/Dashboard';
+import Department from './Pages/Department/Department';
+import Products from './Pages/Products/Products';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Routes>
+    <Route path='/' element = {<Dashboard />} />
+    <Route path='/department' element = {<Department />} />
+    <Route path='/products' element = {<Products />} />
+  </Routes>
     </div>
   );
 }
---
+++ src/Components/Navbar/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import "./NavBar.css";
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <div className='navbar'>
+      <Link to= "/">Dashboard</Link>
+      <Link to= "/department">Departments</Link>
+      <Link to= "/products">Products</Link>
+    </div>
+  )
+}
+
+export default NavBar;
